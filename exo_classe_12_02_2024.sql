375d370042e4b359c228edaa903d0faaf71e0c80
arsene <fopoar@gmail.com>
2024-02-12 13:52:11 -0400

    question 2 : solution finale

--- exo_classe_12_02_2024.sql
+++ exo_classe_12_02_2024.sql
@@ -14,7 +14,7 @@ WHERE p.pub_id IN (SELECT  pub_id
 FROM employees 
 WHERE job_lvl IN ( 'JUNIOR' ,'INTERMEDIAIRE'));
 
--- Quels sont les editeurs employant plus de deux employes de niveau JUNIOR
+-- Quels sont les editeurs employant au moins deux employes de niveau JUNIOR
 -- -- cherchons d'abord les employes de niveau JUNIOR
 SELECT emp_id 
 FROM employees 
@@ -24,3 +24,17 @@ SELECT pub_id, COUNT(emp_id)
 FROM employees 
 WHERE job_lvl = 'JUNIOR'
 GROUP BY(pub_id);
+-- Cherchons ceux de ces employeurs ayant au moins deux JUNIORS
+SELECT pub_id, COUNT(emp_id) as nb_junior
+FROM employees 
+WHERE job_lvl = 'JUNIOR'
+GROUP BY(pub_id)
+HAVING nb_junior >=2 ;
+-- Donnons les noms des empoyeurs concernes
+SELECT pub_name 
+FROM publishers p
+WHERE p.pub_id IN (SELECT pub_id
+FROM employees 
+WHERE job_lvl = 'JUNIOR'
+GROUP BY(pub_id)
+HAVING COUNT(emp_id)  >=2 );
