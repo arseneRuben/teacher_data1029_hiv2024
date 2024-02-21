6d2d3331b6dc8149ffbb0ae8b18322dbd42692a5
arsene <fopoar@gmail.com>
2024-02-21 08:55:33 -0400

    table jobs

--- solution_devoir_semaine_5.sql
+++ solution_devoir_semaine_5.sql
@@ -4,11 +4,13 @@ create database if not exists library2;
 use library2;
 
 -- table Authors
+drop table if exists Authors;
 create table Authors (
-    au_id TINYINT auto_increment primary key,
+    au_id TINYINT  primary key,
     au_lname varchar(50),
     au_fname varchar(50),
-    phone varchar(20) unique check (phone like '+%' and length(phone) between 10 and 15),
+	phone varchar(20) unique check (phone like '+%' and length(phone) between 10 and 15),
+	-- phone varchar(20) unique check (phone rlike '^+[0-9]{10,15}$'),
     address varchar(50),
     city varchar(50),
     state varchar(50),
@@ -16,4 +18,12 @@ create table Authors (
     zip varchar(6) check (zip regexp '^[A-Za-z][0-9][A-Za-z][0-9][A-Za-z][0-9]$'),
     contract text,
     email varchar(50) unique check (email like '%@%')
+);
+
+-- table Jobs
+create table Jobs (
+    job_id tinyint auto_increment primary key,
+    job_desc varchar(50),
+    min_lvl enum('Stagiaire', 'Junior', 'Intermediaire', 'Seinior'),
+    max_lvl enum('Stagiaire', 'Junior', 'Intermediaire', 'Seinior')
 );
