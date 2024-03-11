4cfa57f3adcef65b31dfdd366c1b698df2d1f43d
arsene <fopoar@gmail.com>
2024-03-11 13:59:55 -0300

    question 2 a 6

--- tp_user_management/solution.sql
+++ tp_user_management/solution.sql
@@ -1,5 +1,12 @@
 
 -- 2 Creer les utilisateurs
-CREATE USER "librarian"@"localhost" IDENTIFIED BY "PasswordLibrarian";
-CREATE USER "pharmacist"@"localhost" IDENTIFIED BY "PasswordPharmacist";
+CREATE USER IF NOT EXISTS "librarian"@"localhost" IDENTIFIED BY "PasswordLibrarian";
+CREATE USER IF NOT EXISTS "pharmacist"@"localhost" IDENTIFIED BY "PasswordPharmacist";
+-- 3 On ne leur a pas accorde les droits necessaires, mais root a ces droits par defaut
+-- 5 Non
 
+-- 6
+GRANT SELECT, UPDATE, DELETE, INSERT ON epharmacy.role 	TO "pharmacist"@"localhost";
+GRANT SELECT, UPDATE, DELETE, INSERT ON epharmacy.user 	TO "pharmacist"@"localhost";
+GRANT SELECT, UPDATE, DELETE, INSERT ON epharmacy.supplier 	TO "pharmacist"@"localhost";
+GRANT SELECT, UPDATE, DELETE, INSERT ON epharmacy.connection_history 	TO "pharmacist"@"localhost";
